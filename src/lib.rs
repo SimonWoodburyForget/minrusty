096c30b9d92fd8730e4ce08bb1adc36808ec4408
Simon <simonwoodburyforget@gmail.com>
2020-01-15 18:07:49 -0500

    questionably working RenderSystem

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
+use glow::*;
 use specs::prelude::*;
+use std::{
+    thread::sleep,
+    time::Duration,
+};
 
-mod components;
 mod systems;
 mod resources;
+mod components;
 
 use systems::{
-    RenderSystem,
-    EventSystem,
+    RenderSystem
 };
 
 pub fn main() {
-    let (context, event_pump, glsl_version) = {
+    // Create a context from a sdl2 window
+    #[cfg(feature = "window-sdl2")]
+    let (gl, mut events_loop, render_loop, shader_version, _gl_context, window) = {
         let sdl = sdl2::init().unwrap();
         let video = sdl.video().unwrap();
         let gl_attr = video.gl_attr();
@@ -26,17 +32,31 @@ pub fn main() {
         let gl_context = window.gl_create_context().unwrap();
         let context =
             glow::Context::from_loader_function(|s| video.gl_get_proc_address(s) as *const _);
-        let render_loop = glow::RenderLoop::<sdl2::video::Window>::from_sdl_window(window);
-        let event_pump = sdl.event_pump().unwrap();
-        // (context, event_loop, render_loop, "#version 410", gl_context)
-        let glsl_version = "#version 410";
-        (context, event_pump, glsl_version)
+        // let render_loop = glow::RenderLoop::<sdl2::video::Window>::from_sdl_window(window);
+        let event_loop = sdl.event_pump().unwrap();
+        (context, event_loop, 0, "#version 410", gl_context, window)
     };
-    
+
+
     let mut world = World::new();
     let mut dispatcher = DispatcherBuilder::new()
-        .with_thread_local(RenderSystem::new(context, glsl_version))
-        .with_thread_local(EventSystem(event_pump))
+        .with_thread_local(RenderSystem::new(gl, shader_version))
         .build();
     dispatcher.setup(&mut world);
+    
+    let mut running = true;
+    while running {
+        dispatcher.dispatch(&world);
+        world.maintain();
+        
+        window.gl_swap_window();        
+        for event in events_loop.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => running = false,
+                _ => {}
+            }
+        }
+
+        sleep(Duration::from_millis(50));
+    }
 }
--- src/systems.rs
+++ src/systems.rs
@@ -39,11 +39,11 @@ use sdl2::{
 // }
 
 /// Render system for OpenGL graphics processing.
-pub struct RenderSystem; // {
-//     gl: Context,
-//     va: u32,
-//     pg: u32,
-// }
+pub struct RenderSystem {
+    gl: Context,
+    va: u32,
+    pg: u32,
+}
 
 impl RenderSystem {
     /// init gl shaders/programs/arrays/... 
@@ -54,64 +54,60 @@ impl RenderSystem {
             let pg = gl.create_program().expect("Cannot create program");
             (va, pg)
         };
-        Self
-        // let shader_sources = [
-        //     (glow::VERTEX_SHADER, include_str!("shaders/vss.glsl")),
-        //     (glow::FRAGMENT_SHADER, include_str!("shaders/fss.glsl")),
-        // ];
-        
-        // let mut shaders = Vec::with_capacity(shader_sources.len());
-        
-        // for (shader_type, shader_source) in shader_sources.iter() {
-        //     unsafe {
-        //         let shader = gl.create_shader(*shader_type).expect("Cannot create shader");
-        //         gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
-        //         gl.compile_shader(shader);
-        //         if !gl.get_shader_compile_status(shader) {
-        //             panic!(gl.get_shader_info_log(shader));
-        //         }
-        //         gl.attach_shader(program, shader);
-        //         shaders.push(shader);
-        //     }
-        // }
-        
-        // unsafe {
-        //     gl.link_program(program);
-        //     if !gl.get_program_link_status(program) {
-        //         panic!(gl.get_program_info_log(program));
-        //     }
-            
-        //     for shader in shaders {
-        //         gl.detach_shader(program, shader);
-        //         gl.delete_shader(shader);
-        //     }
-            
-        //     gl.use_program(Some(program));
-        //     gl.clear_color(0.1, 0.2, 0.3, 1.0);
-        // }
 
-        // Self {
-        //     gl: gl,
-        //     va: vertex_array,
-        //     pg: program,
-        // }
+        let shader_sources = [
+            (glow::VERTEX_SHADER, include_str!("shaders/vss.glsl")),
+            (glow::FRAGMENT_SHADER, include_str!("shaders/fss.glsl")),
+        ];
+        
+        let mut shaders = Vec::with_capacity(shader_sources.len());
+        
+        for (shader_type, shader_source) in shader_sources.iter() {
+            unsafe {
+                let shader = gl.create_shader(*shader_type).expect("Cannot create shader");
+                gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
+                gl.compile_shader(shader);
+                if !gl.get_shader_compile_status(shader) {
+                    println!("{}", gl.get_shader_info_log(shader));
+                    panic!(gl.get_shader_info_log(shader));
+                }
+                gl.attach_shader(program, shader);
+                shaders.push(shader);
+            }
+        }
+
+        unsafe {
+            gl.link_program(program);
+            if !gl.get_program_link_status(program) {
+                panic!(gl.get_program_info_log(program));
+            }
+            
+            for shader in shaders {
+                gl.detach_shader(program, shader);
+                gl.delete_shader(shader);
+            }
+            
+            gl.use_program(Some(program));
+            gl.clear_color(0.1, 0.2, 0.3, 1.0);
+        }
+
+        Self {
+            gl: gl,
+            va: vertex_array,
+            pg: program,
+        }
     }
 }
 
 impl<'a> System<'a> for RenderSystem {
-    type SystemData = Read<'a, Quit>;
+    type SystemData = ();
 
-    fn run(&mut self, quit: Self::SystemData) {
-        // let RenderSystem { gl, pg, va } = self;
-        // unsafe {
-        //     gl.clear(glow::COLOR_BUFFER_BIT);
-        //     gl.draw_arrays(glow::TRIANGLES, 0, 3);
-        
-        //     if let Quit(true) = *quit {
-        //         gl.delete_program(*pg);
-        //         gl.delete_vertex_array(*va);
-        //     }
-        // }
+    fn run(&mut self, (): Self::SystemData) {
+        let RenderSystem { gl, pg, va } = self;
+        unsafe {
+            gl.clear(glow::COLOR_BUFFER_BIT);
+            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+        }
     }
 }
 
