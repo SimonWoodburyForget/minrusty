d6e95526fb37017eac874ed80c6bce722a9d3a03
Simon <simonwoodburyforget@gmail.com>
2020-01-22 14:30:29 -0500

    dispatcher doesn't compile on wasm

--- src/state.rs
+++ src/state.rs
@@ -2,14 +2,14 @@ use specs::prelude::*;
 
 pub struct GameState {
     esc: World,
-    dis: Dispatcher<'static, 'static>,
+    // dis: Dispatcher<'static, 'static>,
 }
 
 impl GameState {
     pub fn new() -> Self {
         Self {
             esc: World::new(),
-            dis: DispatcherBuilder::new().build(),
+            // dis: DispatcherBuilder::new().build(),
         }
     }
 }
