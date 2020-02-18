929d4bb372a3fc2cb1d2e4be48ec1941e13dc679
Simon <simonwoodburyforget@gmail.com>
2020-02-18 10:03:36 -0500

    refactored get/set arguments

--- volume/src/lib.rs
+++ volume/src/lib.rs
@@ -17,35 +17,40 @@ mod index {
         pub xy: ChunkIndex,
         pub n: TileIndex,
     }
-    
+
     type ChunkIndex = [u32; 2];
     type TileIndex = usize;
-}
 
-fn chunkmap_index(position: [u32; 2]) -> Index {
-    let [x, y] = position;
-    let cx = x % CHUNK_SIZE as u32;
-    let cy = x % CHUNK_SIZE as u32;
-    let n = (x - cx) * (y - cy);
-    Index {
-        xy: [cx, cy],
-        n: n as usize
+    impl From<[u32; 2]> for Index {
+        fn from(x: [u32; 2]) -> Self {
+            chunkmap_index(x)
+        }
+    }
+    
+    fn chunkmap_index(position: [u32; 2]) -> Index {
+        let [x, y] = position;
+        let cx = x % super::CHUNK_SIZE as u32;
+        let cy = y % super::CHUNK_SIZE as u32;
+        let n = (x - cx) * (y - cy);
+        Index {
+            xy: [cx, cy],
+            n: n as usize
+        }
     }
 }
 
 impl<T> SpacialMap<T> {
     /// Get's a tile.
-    pub fn get(&self, xy: [u32; 2]) -> Option<&T> {
-        let Index { xy, n } = chunkmap_index(xy);
+    pub fn get<P: Into<Index>>(&self, xy: P) -> Option<&T> {
+        let Index { xy, n } = xy.into();
         self.chunks.get(&xy).map(|c| &c[n])
     }
 
-    pub fn set(&mut self, xy: [u32; 2], v: T) {
-        let Index { xy, n } = chunkmap_index(xy);
-        // if let Some(chunk) = self.chunks[&cx_cy] {
-        //     chunk.tiles[n] = v
-        // }
+    pub fn set<P: Into<Index>>(&mut self, xy: P, v: T) {
+        let Index { xy, n } = xy.into();
 
-
+        if let Some(chunk) = self.chunks.get_mut(&xy) {
+            chunk[n] = v;
+        }
     }
 }
